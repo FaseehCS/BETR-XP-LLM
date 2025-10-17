172574ddbf5b685417557af6ee1b8c53bbb9da31
FaseehCS <faseeh.ahmad@cs.lth.se>
2025-10-17 14:59:05 +0200

    Config: Calls class decorator for DEMO class

--- interfaces/robotwin_world_interface.py
+++ interfaces/robotwin_world_interface.py
@@ -1,10 +1,20 @@
 from interfaces.base_world_interface import BaseWorldInterface
-from robotwin.envs._base_task import Base_Task
+from envs._base_task import Base_Task
 from collections import defaultdict
 import numpy as np
 
-class RobotwinWorldInterface(Base_Task, BaseWorldInterface): # Methods with same name use the Base_Task implementation
-    def __init__(self, movable_objects=None, graspable_objects=None, table_offset=0, config_kwargs=None):
+# robotwin imports
+from robotwin.collect_data import class_decorator, get_camera_config
+import yaml
+import os
+import traceback
+
+
+TASK_NAME = "pick_obj"   # This is known. Basically we can get from runtime file
+DEMO = class_decorator(TASK_NAME)
+
+class RobotwinWorldInterface(DEMO, BaseWorldInterface): # Methods with same name use the Base_Task implementation
+    def __init__(self, task_name=TASK_NAME, task_config="demo_clean", seed=0, gripper_bias=0.16, movable_objects=None, graspable_objects=None, table_offset=0, config_kwargs=None):
         super().__init__(
             cfree_interface=None,
             movable_objects=movable_objects,
@@ -16,6 +26,120 @@ class RobotwinWorldInterface(Base_Task, BaseWorldInterface): # Methods with same
         self._build_name_to_actor()
         self.grasped_object = None
         self.manipulation_target = None
+        
+        # Create robotwin environment
+        self.args = self.create_args(self, task_name, task_config, seed, gripper_bias)
+        for i in range(seed, seed + 3):
+            try:
+                self.args["seed"] = seed
+                self.setup_demo(**self.args)
+                break
+            except Exception as e:
+                if i == seed + 2:
+                    continue
+                print(f"Failed to create demo for task {self.task_name}: {e}")
+                traceback.print_exc()
+                try:
+                    if self.args["render_freq"]:
+                        self.close_env()
+                        self.viewer.close()
+                except:
+                    pass
+                continue
+            
+        self.run_demo()
+
+    def create_args(self, task_name, task_config="demo_clean", seed=0, gripper_bias=0.16):
+        
+        # task = class_decorator(task_name)
+        config_path = f"./task_config/{task_config}.yml"
+
+        with open(config_path, "r", encoding="utf-8") as f:
+            args = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+        args['task_name'] = task_name
+
+        embodiment_type = args.get("embodiment")
+        embodiment_config_path = os.path.join(CONFIGS_PATH, "_embodiment_config.yml")
+
+        with open(embodiment_config_path, "r", encoding="utf-8") as f:
+            _embodiment_types = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+        def get_embodiment_file(embodiment_type):
+            robot_file = _embodiment_types[embodiment_type]["file_path"]
+            if robot_file is None:
+                raise "missing embodiment files"
+            return robot_file
+
+        if len(embodiment_type) == 1:
+            args["left_robot_file"] = get_embodiment_file(embodiment_type[0])
+            args["right_robot_file"] = get_embodiment_file(embodiment_type[0])
+            args["dual_arm_embodied"] = True
+        elif len(embodiment_type) == 3:
+            args["left_robot_file"] = get_embodiment_file(embodiment_type[0])
+            args["right_robot_file"] = get_embodiment_file(embodiment_type[1])
+            args["embodiment_dis"] = embodiment_type[2]
+            args["dual_arm_embodied"] = False
+        else:
+            raise "number of embodiment config parameters should be 1 or 3"
+
+        args["left_embodiment_config"] = get_embodiment_config(args["left_robot_file"], gripper_bias=gripper_bias)
+        args["right_embodiment_config"] = get_embodiment_config(args["right_robot_file"], gripper_bias=gripper_bias)
+
+        if len(embodiment_type) == 1:
+            embodiment_name = str(embodiment_type[0])
+        else:
+            embodiment_name = str(embodiment_type[0]) + "+" + str(embodiment_type[1])
+
+        # show config
+        print("============= Config =============\n")
+        print("\033[95mMessy Table:\033[0m " + str(args["domain_randomization"]["cluttered_table"]))
+        print("\033[95mRandom Background:\033[0m " + str(args["domain_randomization"]["random_background"]))
+        if args["domain_randomization"]["random_background"]:
+            print(" - Clean Background Rate: " + str(args["domain_randomization"]["clean_background_rate"]))
+        print("\033[95mRandom Light:\033[0m " + str(args["domain_randomization"]["random_light"]))
+        if args["domain_randomization"]["random_light"]:
+            print(" - Crazy Random Light Rate: " + str(args["domain_randomization"]["crazy_random_light_rate"]))
+        print("\033[95mRandom Table Height:\033[0m " + str(args["domain_randomization"]["random_table_height"]))
+        print("\033[95mRandom Head Camera Distance:\033[0m " + str(args["domain_randomization"]["random_head_camera_dis"]))
+
+        print("\033[94mHead Camera Config:\033[0m " + str(args["camera"]["head_camera_type"]) + f", " +
+            str(args["camera"]["collect_head_camera"]))
+        print("\033[94mWrist Camera Config:\033[0m " + str(args["camera"]["wrist_camera_type"]) + f", " +
+            str(args["camera"]["collect_wrist_camera"]))
+        print("\033[94mEmbodiment Config:\033[0m " + embodiment_name)
+        print("\n==================================")
+
+        args["embodiment_name"] = embodiment_name
+        args['task_config'] = task_config
+        args["save_path"] = os.path.join(args["save_path"], str(args["task_name"]), args["task_config"])
+        args["seed"] = seed
+        return args    
+
+    def run_demo(self):
+        """
+        Run one episode of the task demo and return success status.
+        """
+
+        print(f"Running task: {self.task_name}")
+        success = False
+        self.play_once() # if you want to run the task
+        if self.plan_success and self.check_success():
+            print(f"simulate {self.task_name} success!")
+            success = True
+        else:
+            print(f"simulate data episode fail!")
+
+        # if self.args["render_freq"]:
+        #     while not self.viewer.closed:
+        #         self.scene.step()
+        #         self.scene.update_render()
+        #         self.viewer.render()
+
+        #     self.close_env()
+        #     self.viewer.close()
+        
+        return success
 
     # Utility to build a mapping from object names to actors
     def _build_name_to_actor(self):
