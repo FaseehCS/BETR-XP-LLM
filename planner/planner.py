630f550b466789e403d2d1c8f7595eebd70527bc
HashimHS <hashims97@gmail.com>
2025-10-30 17:09:03 +0100

    changes skill_name to condition_name for condition classes

--- planner/planner.py
+++ planner/planner.py
@@ -72,7 +72,7 @@ def handle_precondition(
     for action in action_list:
         skill_descriptions[action.skill_name] = action.description
     for condition in condition_list:
-        condition_descriptions[condition.skill_name] = condition.description
+        condition_descriptions[condition.condition_name] = condition.description
 
     descriptions["skills"] = skill_descriptions
     descriptions["conditions"] = condition_descriptions
